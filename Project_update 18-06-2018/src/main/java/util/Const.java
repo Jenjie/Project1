9135ee525e1fac75e7b5ca6d9747914a7d76ad21
Jenjie <s5704062630014@email.kmutnb.ac.th>
2018-07-23 07:58:29 +0700

    commit 98

--- Project_update 18-06-2018/src/main/java/util/Const.java
+++ Project_update 18-06-2018/src/main/java/util/Const.java
@@ -7,9 +7,9 @@ public class Const {
     public static final boolean opennow = true ;
 
     //parameter API Direction
-    public static final String dis_KEY = "AIzaSyAZkmZb2eX3xez31iSW12gnAUplJ9e5xDM";
+    public static final String dis_KEY = "AIzaSyBGG6vchFVgrXn54qZ5zMEjj7rOFLkZ7gU";
     public static final String units = "metric";
 
     public static final String urlConnect = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net:3306/heroku_bc560e3cff14c41" +
-            "?user=bff338ea9d4e3f&password=42d3c006" ;
+            "?user=bff338ea9d4e3f&password=42d3c006&characterEncoding=UTF-8" ;
 }
